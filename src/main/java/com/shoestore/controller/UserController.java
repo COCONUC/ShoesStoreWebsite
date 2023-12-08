4c35910421b529ac7b6ad801821ef3c9b3d7168e
luxNG <72197267+luxNG@users.noreply.github.com>
2023-12-08 18:14:41 +0700

    git commit -m "SSW-27 <Assigment 3 structure 08_12>"

---
+++ src/main/java/com/shoestore/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.shoestore.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	@GetMapping("/hello-world")
+	public String hello() {
+		return "Hello Ciao";
+	}
+}
---
+++ src/main/java/com/shoestore/main/Prj321xAsm3Sangntfx31391Application.java
@@ -0,0 +1,15 @@
+package com.shoestore.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.shoestore")
+public class Prj321xAsm3Sangntfx31391Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prj321xAsm3Sangntfx31391Application.class, args);
+	}
+
+}
